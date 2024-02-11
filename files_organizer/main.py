eb2ce7102414346c1a358886c56709e91344d99e
colo993 <wakula993@gmail.com>
2024-02-11 18:25:55 +0100

    merge _get_list_of_files methods

--- files_organizer/main.py
+++ files_organizer/main.py
@@ -36,19 +36,13 @@ class File:
             
         return files_in_datetime_range
 
-    def _get_list_of_files_intersection(self, extensions=None, names=None, dates=None) -> list():
+    def _get_list_of_files(self, option, extensions=None, names=None, dates=None) -> list():
         entry_list = [extensions, names, dates] 
-        print(f"Entry_list: {entry_list}")
         filtered_list = [x for x in entry_list if x is not None]
-        
-        return list(set.intersection(*map(set,filtered_list)))
-    
-    def _get_list_of_files_union(self, extensions=None, names=None, dates=None) -> list():
-        entry_list = [extensions, names, dates] 
-        print(f"Entry_list: {entry_list}")
-        filtered_list = [x for x in entry_list if x is not None]
-        
-        return list(set.union(*map(set,filtered_list)))
+        if option == 'intersection':
+            return list(set.intersection(*map(set,filtered_list)))
+        elif option == 'union':
+            return list(set.union(*map(set,filtered_list)))     
 
     def copy(self) -> None:
         for file in self._get_list_of_files():
@@ -71,12 +65,14 @@ class File:
 
 if __name__ == "__main__":
     file = File('/home/colo/files_organizer/sandbox', '/home/colo/files_organizer/files_test/')
-    print(file._get_list_of_files_union(file._filter_by_extension('png'),
+    print(file._get_list_of_files('union',
+                                  file._filter_by_extension('png'),
                                   file._filter_by_name('test'),
                                   file._filter_by_date_creation(datetime(2024, 2, 4, 00, 00, 00), 
                                                           datetime(2024, 2, 8, 12, 00, 00))))
     
-    print(file._get_list_of_files_or(file._filter_by_extension('png'),
+    print(file._get_list_of_files('intersection',
+                                  file._filter_by_extension('png'),
                                   file._filter_by_name('test'),
                                   file._filter_by_date_creation(datetime(2024, 2, 4, 00, 00, 00), 
                                                           datetime(2024, 2, 8, 12, 00, 00))))
