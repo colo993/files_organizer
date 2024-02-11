a7fa0a41d4471c58a13d01f979bfc08832cb70cb
colo993 <wakula993@gmail.com>
2024-02-11 18:22:55 +0100

    working logic - first version

--- files_organizer/main.py
+++ files_organizer/main.py
@@ -1,27 +1,54 @@
 import os
 import shutil
+from datetime import datetime
+import time
+import re
+import json
 
 class File:
     """Copy or move selected files to destination folder"""
-    def __init__(self, source_path, destination_path, index_of_extension):
+    def __init__(self, source_path, destination_path):
         self.source_path = source_path
         self.destination_path = destination_path
-        self.index_of_extension = index_of_extension
 
-    def _get_extension(self) -> list(): #HOW TO SELECT EXTENSION?!
-        """Select extension of a file to being moved or copied"""
-        extensions_list = ['.jpeg', '.png', '.txt']
-        return extensions_list
-    
-    def _get_file_name(self):
-        pass
-    
-    def _get_date_creation(self):
-        pass
-
-    def _get_list_of_files(self) -> list():
+    def _filter_by_extension(self, type_of_extension) -> list(): 
+        """Select extension of a file"""
+        with open('files_organizer/extensions_list.json', 'r') as file:
+            extensions = json.load(file)
+            
         return [file for file in os.listdir(path=self.source_path) if 
-                self.get_extension()[self.index_of_extension] in file.lower()]
+                re.search(extensions[type_of_extension], file.lower())]
+    
+    def _filter_by_name(self, file_name_to_search) -> list():
+        """Find the files which matches the given word"""
+        return [file for file in os.listdir(path=self.source_path) if 
+                file_name_to_search in file]
+    
+    def _filter_by_date_creation(self, from_time, to_time) -> list():
+        """Find the files created in between the given datetime range"""
+        files_in_datetime_range = []
+        for file in os.listdir(path=self.source_path):
+            time_in_epoch = os.path.getmtime(f"{self.source_path}/{file}")
+            time_in_str =  time.ctime(time_in_epoch)
+            datetime_creation = datetime.strptime(time_in_str, "%a %b %d %H:%M:%S %Y")
+            if datetime_creation >= from_time and datetime_creation <= to_time:
+                files_in_datetime_range.append(file)
+            
+        return files_in_datetime_range
+
+    def _get_list_of_files_intersection(self, extensions=None, names=None, dates=None) -> list():
+        entry_list = [extensions, names, dates] 
+        print(f"Entry_list: {entry_list}")
+        filtered_list = [x for x in entry_list if x is not None]
+        
+        return list(set.intersection(*map(set,filtered_list)))
+    
+    def _get_list_of_files_union(self, extensions=None, names=None, dates=None) -> list():
+        entry_list = [extensions, names, dates] 
+        print(f"Entry_list: {entry_list}")
+        filtered_list = [x for x in entry_list if x is not None]
+        
+        return list(set.union(*map(set,filtered_list)))
 
     def copy(self) -> None:
         for file in self._get_list_of_files():
@@ -43,7 +70,14 @@ class File:
 
 
 if __name__ == "__main__":
-    file = File('sandbox', 'files_test/', 2)
-    print(file.get_extension())
-    file.copy()
+    file = File('/home/colo/files_organizer/sandbox', '/home/colo/files_organizer/files_test/')
+    print(file._get_list_of_files_union(file._filter_by_extension('png'),
+                                  file._filter_by_name('test'),
+                                  file._filter_by_date_creation(datetime(2024, 2, 4, 00, 00, 00), 
+                                                          datetime(2024, 2, 8, 12, 00, 00))))
+    
+    print(file._get_list_of_files_or(file._filter_by_extension('png'),
+                                  file._filter_by_name('test'),
+                                  file._filter_by_date_creation(datetime(2024, 2, 4, 00, 00, 00), 
+                                                          datetime(2024, 2, 8, 12, 00, 00))))
     
