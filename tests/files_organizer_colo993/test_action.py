ff1c48b01ab43f149b2f7b0091b5ac6e022760b4
colo993 <wakula993@gmail.com>
2024-03-01 14:01:58 +0100

    remove test_files and add test for move/copy function

--- tests/files_organizer_colo993/test_action.py
+++ tests/files_organizer_colo993/test_action.py
@@ -2,36 +2,67 @@ import os
 import shutil
 import tempfile
 
+import pytest
+
 from files_organizer_colo993 import action
 
+
 class TestFile:
-    
-    def test_copy(self):
-        with tempfile.TemporaryDirectory() as source_dir, tempfile.TemporaryDirectory() as dest_dir:
-            files_list = ["test_file1.txt", "test_file2.txt", "test_file3.txt"]
-            for file in files_list:
-                with open(os.path.join(source_dir, file), "w") as f:
-                    f.write("Test")
-            
-            file_object = action.File(files_list, source_dir, dest_dir)
-            
-            file_object.copy()
-            
-            copied_files_list = os.listdir(dest_dir)
-            assert sorted(copied_files_list) == sorted(files_list)
-    
-    def test_move(self):
-        with tempfile.TemporaryDirectory() as source_dir, tempfile.TemporaryDirectory() as dest_dir:
-            files_list = ["test_file1.txt", "test_file2.txt", "test_file3.txt"]
-            for file in files_list:
-                with open(os.path.join(source_dir, file), "w") as f:
-                    f.write("Test")
-            
-            file_object = action.File(files_list, source_dir, dest_dir)
-            
-            file_object.move()
-            
-            moved_files_source = os.listdir(source_dir)
-            moved_files_dest =os.listdir(dest_dir)
-            assert len(moved_files_source) == 0
-            assert sorted(moved_files_dest) == sorted(files_list)
+    @pytest.fixture
+    def mock_files(self, tmpdir):
+        """
+        Create mock files for testing.
+        """
+        files = ["file1.txt", "file2.txt", "file3.txt"]
+        for file in files:
+            path = tmpdir.join(file)
+            path.write("")
+        return tmpdir
+
+    @pytest.fixture
+    def file_instance(self, tmpdir):
+        """
+        Initialize File object for testing.
+        """
+        source_path = str(tmpdir)
+        destination_path = str(tmpdir.join("destination"))
+        os.makedirs(destination_path)  # Create a destination directory
+        list_of_files = ["file1.txt", "file2.txt"]
+        for file in list_of_files:
+            file_path = os.path.join(source_path, file)
+            with open(file_path, "w") as f:
+                f.write("Test content")
+        return action.File(list_of_files, source_path, destination_path)
+
+    def test_copy(self, file_instance):
+        """
+        Test copy method.
+        """
+        file_instance.copy()
+        destination_files = os.listdir(file_instance.destination_path)
+        assert len(destination_files) == 2
+        assert all(file in destination_files for file in file_instance.list_of_files)
+
+    def test_copy_no_permissions(self, file_instance):
+        os.chmod(file_instance.destination_path, 0o400)
+
+        with pytest.raises(IOError):
+            file_instance.copy()
+
+    def test_move(self, file_instance):
+        """
+        Test copy method.
+        """
+        file_instance.move()
+        destination_files = os.listdir(file_instance.destination_path)
+        assert len(destination_files) == 2
+        assert all(file in destination_files for file in file_instance.list_of_files)
+
+        source_files = os.listdir(file_instance.source_path)
+        assert len(source_files) == 1
+
+    def test_move_no_permissions(self, file_instance):
+        os.chmod(file_instance.destination_path, 0o400)
+
+        with pytest.raises(IOError):
+            file_instance.move()
--- tests/files_organizer_colo993/test_filters.py
+++ tests/files_organizer_colo993/test_filters.py
@@ -6,50 +6,48 @@ import pytest
 
 from files_organizer_colo993 import filters
 
-# Mock extensions data for testing
-def get_extensions_data():
-    return {
-        "jpg" : r"jp(.*)",
-        "png" : r"png$",
-        "bmp" : r"bmp$"
-    }
-
 
 class TestFilesList:
     @pytest.fixture
     def files_list_instance(self, tmpdir):
         tmpdir_path = str(tmpdir)
         files_list = filters.FilesList(tmpdir_path)
-        file_names = ["basic_file1.jpg", "ExtEnded_file2.jpg", 
-                      "basic_file3.png", "ExtEnded_file4.png", 
-                      "basic_file5.bmp", "ExtEnded_file6.bmp"
+        file_names = [
+            "basic_file1.jpg",
+            "ExtEnded_file2.jpg",
+            "basic_file3.png",
+            "ExtEnded_file4.png",
+            "basic_file5.bmp",
+            "ExtEnded_file6.bmp",
         ]
         for i, file in enumerate(file_names, start=1):
             file_path = os.path.join(tmpdir_path, file)
             with open(file_path, "a") as f:
-               f.write('Test')
+                f.write("Test")
             os.utime(file_path, (i, i))
-            
-        return files_list
-    
+
+        yield files_list
+
+        tmpdir.remove()
+
     def test_filter_by_extension_jpg(self, files_list_instance):
         result = files_list_instance.filter_by_extension("jpg")
         assert len(result) == 2
         assert all(re.search(r"jp(.*)", file) for file in result)
-    
+
     def test_filter_by_extension_invalid_input(self, files_list_instance):
         with pytest.raises(KeyError):
             files_list_instance.filter_by_extension("Invalid_input")
-    
+
     def test_filter_by_name_basic(self, files_list_instance):
         result = files_list_instance.filter_by_name("basic")
         assert len(result) == 3
         assert all(re.search(r"basic", file) for file in result)
-    
+
     def test_filter_by_name_invalid_input(self, files_list_instance):
         with pytest.raises(TypeError):
             files_list_instance.filter_by_name(1)
-    
+
     def test_filter_by_date_creation(self, files_list_instance):
         from_time = datetime.fromtimestamp(1)
         print(from_time)
@@ -57,30 +55,32 @@ class TestFilesList:
         print(to_time)
         result = files_list_instance.filter_by_date_creation(from_time, to_time)
         assert len(result) == 2
-        assert ['basic_file1.jpg', 'ExtEnded_file2.jpg'] == result
-    
+        assert ["basic_file1.jpg", "ExtEnded_file2.jpg"] == result
+
     def test_filter_by_date_creation_invalid_input(self, files_list_instance):
         with pytest.raises(TypeError):
             files_list_instance.filter_by_date_creation("Invalid", 123)
-            
+
     def test_get_intersection(self, files_list_instance):
-        extension = files_list_instance.filter_by_extension('png')
-        names = files_list_instance.filter_by_name('ExtEnded')
+        extension = files_list_instance.filter_by_extension("png")
+        names = files_list_instance.filter_by_name("ExtEnded")
         dates = files_list_instance.filter_by_date_creation(
-                                                    datetime.fromtimestamp(1), 
-                                                    datetime.fromtimestamp(6))
-        assert files_list_instance.get('intersection', 
-                                       extension, 
-                                       names, 
-                                       dates) == ["ExtEnded_file4.png"]
-    
+            datetime.fromtimestamp(1), datetime.fromtimestamp(6)
+        )
+        assert files_list_instance.get("intersection", extension, names, dates) == [
+            "ExtEnded_file4.png"
+        ]
+
     def test_get_union(self, files_list_instance):
-        extension = files_list_instance.filter_by_extension('png')
-        names = files_list_instance.filter_by_name('ExtEnded')
+        extension = files_list_instance.filter_by_extension("png")
+        names = files_list_instance.filter_by_name("ExtEnded")
         dates = files_list_instance.filter_by_date_creation(
-                                                    datetime.fromtimestamp(3), 
-                                                    datetime.fromtimestamp(4))
-        result = sorted(files_list_instance.get('union', extension, names, dates))
-        assert result == ['ExtEnded_file2.jpg', 'ExtEnded_file4.png', 
-                          'ExtEnded_file6.bmp', 'basic_file3.png']
-        
+            datetime.fromtimestamp(3), datetime.fromtimestamp(4)
+        )
+        result = sorted(files_list_instance.get("union", extension, names, dates))
+        assert result == [
+            "ExtEnded_file2.jpg",
+            "ExtEnded_file4.png",
+            "ExtEnded_file6.bmp",
+            "basic_file3.png",
+        ]
