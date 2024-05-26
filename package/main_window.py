35e7a6b663e5109caa6ce8cdd73baa29cff32b52
colo993 <wakula993@gmail.com>
2024-05-26 16:59:16 +0200

    refactoring variables and method names in FilesOrganizerMainWindow

--- package/main_window.py
+++ package/main_window.py
@@ -35,212 +35,212 @@ class FilesOrganizerWindow(QMainWindow):
     def __init__(self):
         """Initis FilesOrganizer window with GUI."""
         super().__init__()
-        self._initUI()
-        self._createMenu()
-        self._createWidgets()
+        self._init_ui()
+        self._create_menu()
+        self._create_widgets()
     
-    def _initUI(self):
+    def _init_ui(self):
         """Set window title and size."""
         self.setWindowTitle("Files Organizer")
         self.setGeometry(800, 800, 500, 600)
         
-    def _createMenu(self):
+    def _create_menu(self):
         """Add menu bar."""
-        toolbar = QToolBar("My main toolbar.")
-        self.addToolBar(toolbar)
+        self._toolbar = QToolBar("My main toolbar.")
+        self.addToolBar(self._toolbar)
         
-        new_button = QAction("&New", self)
-        new_button.triggered.connect(self._mainWindow)
+        self._reset_to_default_button = QAction("&New", self)
+        self._reset_to_default_button.triggered.connect(self._mainWindow)
         
-        exit_button = QAction("&Exit", self)
-        exit_button.triggered.connect(self.close)
+        self._exit_button = QAction("&Exit", self)
+        self._exit_button.triggered.connect(self.close)
         
-        how_to_use_button = QAction("&How to use", self)
-        how_to_use_button.triggered.connect(self._howtouseWindow)
+        self._how_to_use_button = QAction("&How to use", self)
+        self._how_to_use_button.triggered.connect(self._howtouseWindow)
         
-        about_button = QAction("&About", self)
-        about_button.triggered.connect(self._aboutWindow)
+        self._about_button = QAction("&About", self)
+        self._about_button.triggered.connect(self._aboutWindow)
         
-        file_menu = self.menuBar().addMenu("&File")
-        file_menu.addAction(new_button)
-        file_menu.addSeparator()
-        file_menu.addAction(exit_button)
+        self._file_menu = self.menuBar().addMenu("&File")
+        self._file_menu.addAction(self._reset_to_default_button)
+        self._file_menu.addSeparator()
+        self._file_menu.addAction(self._exit_button)
         
-        help_menu = self.menuBar().addMenu("&Help")
-        help_menu.addAction(how_to_use_button)
-        help_menu.addSeparator()
-        help_menu.addAction(about_button)
+        self._help_menu = self.menuBar().addMenu("&Help")
+        self._help_menu.addAction(self._how_to_use_button)
+        self._help_menu.addSeparator()
+        self._help_menu.addAction(self._about_button)
     
-    def _createWidgets(self):
+    def _create_widgets(self):
         """Add widgets."""
-        self.widget = QWidget()
+        widget = QWidget()
         main_layout = QVBoxLayout()
-        self.checkbox_layout_name = QVBoxLayout() 
-        self.checkbox_layout_extension = QVBoxLayout() 
-        self.checkbox_layout_date = QVBoxLayout()
-        self.radiobutton_layout = QGridLayout()
-        self.summary_layout = QVBoxLayout()
+        self._checkbox_layout_name = QVBoxLayout() 
+        self._checkbox_layout_extension = QVBoxLayout() 
+        self._checkbox_layout_date = QVBoxLayout()
+        radiobutton_layout = QGridLayout()
+        summary_layout = QVBoxLayout()
         
         button_source_dir = QPushButton("Select source directory")
         button_destination_dir = QPushButton("Select destination directory")
-        button_source_dir.clicked.connect(self.get_source_directory)
-        button_destination_dir.clicked.connect(self.get_destination_directory)
+        button_source_dir.clicked.connect(self._get_source_directory)
+        button_destination_dir.clicked.connect(self._get_destination_directory)
         
-        self.label_source_dir = QLabel()
-        self.label_destination_dir = QLabel()
+        self._label_source_dir = QLabel()
+        self._label_destination_dir = QLabel()
         
-        self.filter_by_name_checkbox = QCheckBox("Filter by name", self)
-        self.filter_by_name_checkbox.toggled.connect(self.get_file_name)
-        self.filter_by_extension_checkbox = QCheckBox("Filter by extension", self)
-        self.filter_by_extension_checkbox.toggled.connect(self.get_extension)
-        self.filter_by_date_checkbox = QCheckBox("Filter by date range", self)
-        self.filter_by_date_checkbox.toggled.connect(self.get_dates)
+        self._filter_by_name_checkbox = QCheckBox("Filter by name", self)
+        self._filter_by_name_checkbox.toggled.connect(self._get_file_name)
+        self._filter_by_extension_checkbox = QCheckBox("Filter by extension", self)
+        self._filter_by_extension_checkbox.toggled.connect(self._get_extension)
+        self._filter_by_date_checkbox = QCheckBox("Filter by date range", self)
+        self._filter_by_date_checkbox.toggled.connect(self._get_dates)
         
-        self.merged_group = QButtonGroup(self.widget)
-        self.radiobutton_union = QRadioButton("Union")
-        self.merged_group.addButton(self.radiobutton_union, 0)
-        self.radiobutton_union.toggled.connect(self.get_merged_type)
-        self.radiobutton_intersection = QRadioButton("Intersection")
-        self.merged_group.addButton(self.radiobutton_intersection, 1)
-        self.radiobutton_intersection.toggled.connect(self.get_merged_type)
+        merged_group = QButtonGroup(widget)
+        radiobutton_union = QRadioButton("Union")
+        merged_group.addButton(radiobutton_union, 0)
+        radiobutton_union.toggled.connect(self._get_merged_type)
+        radiobutton_intersection = QRadioButton("Intersection")
+        merged_group.addButton(radiobutton_intersection, 1)
+        radiobutton_intersection.toggled.connect(self._get_merged_type)
         
-        self.action_group = QButtonGroup(self.widget)
-        self.radiobutton_copy = QRadioButton("Copy")
-        self.action_group.addButton(self.radiobutton_copy, 2)
-        self.radiobutton_copy.toggled.connect(self.copy_or_move_action)
-        self.radiobutton_move = QRadioButton("Move")
-        self.action_group.addButton(self.radiobutton_move, 3)
-        self.radiobutton_move.toggled.connect(self.copy_or_move_action)
+        action_group = QButtonGroup(widget)
+        radiobutton_copy = QRadioButton("Copy")
+        action_group.addButton(radiobutton_copy, 2)
+        radiobutton_copy.toggled.connect(self._copy_or_move_action)
+        radiobutton_move = QRadioButton("Move")
+        action_group.addButton(radiobutton_move, 3)
+        radiobutton_move.toggled.connect(self._copy_or_move_action)
         
         summary_label = QLabel()
         summary_label.setText("Files to be copied or moved:")
-        self.files_list_label = QLabel()
-        self.files_list_label.setText(self.get_list_of_files())
-        self.run_action_button = QPushButton("RUN")
-        self.run_action_button.clicked.connect(self.start_process)
+        files_list_label = QLabel()
+        files_list_label.setText(self._get_list_of_files())
+        run_action_button = QPushButton("RUN")
+        run_action_button.clicked.connect(self._start_process)
         
         main_layout.addWidget(button_source_dir)
-        main_layout.addWidget(self.label_source_dir)
+        main_layout.addWidget(self._label_source_dir)
         main_layout.addWidget(button_destination_dir)
-        main_layout.addWidget(self.label_destination_dir)
+        main_layout.addWidget(self._label_destination_dir)
         
-        main_layout.addLayout(self.checkbox_layout_name)
-        self.checkbox_layout_name.addWidget(self.filter_by_name_checkbox)
+        main_layout.addLayout(self._checkbox_layout_name)
+        self._checkbox_layout_name.addWidget(self._filter_by_name_checkbox)
         
-        main_layout.addLayout(self.checkbox_layout_extension)
-        self.checkbox_layout_extension.addWidget(self.filter_by_extension_checkbox)
+        main_layout.addLayout(self._checkbox_layout_extension)
+        self._checkbox_layout_extension.addWidget(self._filter_by_extension_checkbox)
         
-        main_layout.addLayout(self.checkbox_layout_date)
-        self.checkbox_layout_date.addWidget(self.filter_by_date_checkbox)
+        main_layout.addLayout(self._checkbox_layout_date)
+        self._checkbox_layout_date.addWidget(self._filter_by_date_checkbox)
         
-        main_layout.addLayout(self.radiobutton_layout)
-        self.radiobutton_layout.addWidget(self.radiobutton_union, 0, 0)
-        self.radiobutton_layout.addWidget(self.radiobutton_intersection, 0, 1)
-        self.radiobutton_layout.addWidget(self.radiobutton_copy, 1, 0)
-        self.radiobutton_layout.addWidget(self.radiobutton_move, 1, 1)
+        main_layout.addLayout(radiobutton_layout)
+        radiobutton_layout.addWidget(radiobutton_union, 0, 0)
+        radiobutton_layout.addWidget(radiobutton_intersection, 0, 1)
+        radiobutton_layout.addWidget(radiobutton_copy, 1, 0)
+        radiobutton_layout.addWidget(radiobutton_move, 1, 1)
         
-        main_layout.addLayout(self.summary_layout)
-        self.summary_layout.addWidget(summary_label)
-        self.summary_layout.addWidget(self.files_list_label)
-        self.summary_layout.addWidget(self.run_action_button)
+        main_layout.addLayout(summary_layout)
+        summary_layout.addWidget(summary_label)
+        summary_layout.addWidget(files_list_label)
+        summary_layout.addWidget(run_action_button)
         
         main_layout.addStretch()
         main_layout.setContentsMargins(10, 10, 20, 30)
-        self.widget.setLayout(main_layout)
-        self.setCentralWidget(self.widget)
+        widget.setLayout(main_layout)
+        self.setCentralWidget(widget)
         
-    def get_source_directory(self):
+    def _get_source_directory(self):
         """Obtain source directory path and print it out on the screen."""
         source_directory = QFileDialog.getExistingDirectory(
                                             self, 
                                             "Source directory",
-                                            self.label_source_dir.text())
+                                            self._label_source_dir.text())
         if source_directory:
-            self.label_source_dir.setText(source_directory)
+            self._label_source_dir.setText(source_directory)
     
-    def get_destination_directory(self):
+    def _get_destination_directory(self):
         """Obtain destination directory path and print it out on the screen."""
         destination_directory = QFileDialog.getExistingDirectory(
                                             self, 
                                             "Destination directory",
-                                            self.label_destination_dir.text())
+                                            self._label_destination_dir.text())
         if destination_directory:
-            self.label_destination_dir.setText(destination_directory)
+            self._label_destination_dir.setText(destination_directory)
         
     
-    def get_file_name(self):
-        """Activate line editor when Checkbox filter_by_name is checked
+    def _get_file_name(self):
+        """Activate line editor when Checkbox filter_by_name_widget is checked
         to type file name for filtering.
         """
-        if self.filter_by_name_checkbox.isChecked():
-            self.filter_by_name = QLineEdit()
-            self.checkbox_layout_name.addWidget(self.filter_by_name)
+        if self._filter_by_name_checkbox.isChecked():
+            self._filter_by_name_widget = QLineEdit()
+            self._checkbox_layout_name.addWidget(self._filter_by_name_widget)
         else:
-            i = self.checkbox_layout_name.indexOf(self.filter_by_name)
-            self.checkbox_layout_name.takeAt(i)
-            self.filter_by_name.deleteLater()
+            i = self._checkbox_layout_name.indexOf(self._filter_by_name_widget)
+            self._checkbox_layout_name.takeAt(i)
+            self._filter_by_name_widget.deleteLater()
     
-    def get_extension(self):
+    def _get_extension(self):
         """Activate combobox with list of extensions when Checkbox 
         filter_by_extension is checked to chose type of extension 
         for filtering.
         """
-        if self.filter_by_extension_checkbox.isChecked():
-            self.filter_by_extension = QComboBox()
-            self.filter_by_extension.addItems(["jpg", "png", "bmp"])
-            self.checkbox_layout_extension.addWidget(self.filter_by_extension)
+        if self._filter_by_extension_checkbox.isChecked():
+            self._filter_by_extension_widget = QComboBox()
+            self._filter_by_extension_widget.addItems(["jpg", "png", "bmp"])
+            self._checkbox_layout_extension.addWidget(self._filter_by_extension_widget)
         else:
-            i = self.checkbox_layout_extension.indexOf(self.filter_by_extension)
-            self.checkbox_layout_extension.takeAt(i)
-            self.filter_by_extension.deleteLater()
+            i = self._checkbox_layout_extension.indexOf(self._filter_by_extension_widget)
+            self._checkbox_layout_extension.takeAt(i)
+            self._filter_by_extension_widget.deleteLater()
 
-    def get_dates(self):
+    def _get_dates(self):
         """Activate datetime widget when Checkbox filter_by_date is 
         checked to chose datetime range for filtering.
         """
-        if self.filter_by_date_checkbox.isChecked():
-            self.to_date_label = QLabel()
-            self.to_date_label.setText("From date time:")
-            self.filter_by_to_date = QDateTimeEdit(calendarPopup=True, 
-                                                   dateTime=datetime.now())
-            self.from_date_label = QLabel()
-            self.from_date_label.setText("To date time:")
-            self.filter_by_from_date = QDateTimeEdit(calendarPopup=True,
+        if self._filter_by_date_checkbox.isChecked():
+            self._to_date_label = QLabel()
+            self._to_date_label.setText("From date time:")  
+            self._filter_to_date_widget = QDateTimeEdit(calendarPopup=True, 
+                                                   dateTime=datetime.now()) 
+            self._from_date_label = QLabel()
+            self._from_date_label.setText("To date time:")    
+            self._filter_from_date_widget = QDateTimeEdit(calendarPopup=True,
                                                      dateTime=datetime.now())
-            self.checkbox_layout_date.addWidget(self.to_date_label)
-            self.checkbox_layout_date.addWidget(self.filter_by_to_date)
-            self.checkbox_layout_date.addWidget(self.from_date_label)
-            self.checkbox_layout_date.addWidget(self.filter_by_from_date)
+            self._checkbox_layout_date.addWidget(self._to_date_label)
+            self._checkbox_layout_date.addWidget(self._filter_to_date_widget)
+            self._checkbox_layout_date.addWidget(self._from_date_label)
+            self._checkbox_layout_date.addWidget(self._filter_from_date_widget)
             
         else:
-            widgets = [self.filter_by_to_date, self.filter_by_from_date, 
-                       self.to_date_label, self.from_date_label]
-            
-            for widget in widgets:
-                widget_index = self.checkbox_layout_date.indexOf(widget)
-                self.checkbox_layout_date.takeAt(widget_index)
-                widget.deleteLater()
+            list_of_date_widgets = [self._filter_to_date_widget, self._filter_from_date_widget, 
+                       self._to_date_label, self._from_date_label]
+            for date_widget in list_of_date_widgets:
+                widget_index = self._checkbox_layout_date.indexOf(date_widget)
+                self._checkbox_layout_date.takeAt(widget_index)
+                date_widget.deleteLater()
     
-    def get_merged_type(self):
+    def _get_merged_type(self):
         """Obtain value of radio buttons Union or Intersection."""
         self.merged_type = self.sender()
         
-    def copy_or_move_action(self):
+    def _copy_or_move_action(self):
         """Obtain value of radio buttons Copy or Move."""
         self.action_type = self.sender()
     
-    def get_list_of_files(self):
+    def _get_list_of_files(self):
         """Return list of files to be copied or moved according to
         chosen option by the user.
         """
+        #TODO
         return "List of files here!!!"
     
-    def start_process(self):
-        """Execute copy or move files after clicking button."""
-        self.files_list = filters.FilesList(self.label_source_dir.text())
-        filtered_list = self.files_list.filter_by_name(self.filter_by_name.text())
+    def _start_process(self):
+        """Copy or move selected files after clicking the button."""
+        files_list = filters.FilesList(self._label_source_dir.text())
+        filtered_list = files_list.filter_by_name_widget(self._filter_by_name_widget.text())
         
-        file = actions.Files(filtered_list, self.label_source_dir.text(), self.label_destination_dir.text())
+        file = actions.Files(filtered_list, self._label_source_dir.text(), self._label_destination_dir.text())
         file.copy()
         
      
