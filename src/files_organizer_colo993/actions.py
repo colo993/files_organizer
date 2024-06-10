15d679f882da092d74c95082a820409bdd496de1
colo993 <wakula993@gmail.com>
2024-06-10 16:27:03 +0200

    reorganize FilesList class package files_organizer_colo993 and add coment in copy/move methods

--- src/files_organizer_colo993/actions.py
+++ src/files_organizer_colo993/actions.py
@@ -20,7 +20,7 @@ class Files:
                 print(f"Permission error copying {file} from \
                       {self.source_path} to {self.destination_path}")
             except IOError:
-                os.umask(0)
+                os.umask(0) # set up 755 chmod permission on created directory
                 os.makedirs(os.path.dirname(f"{self.destination_path}/{file}"), 
                             exist_ok=True)
                 shutil.copy(f"{self.source_path}/{file}", 
@@ -36,7 +36,7 @@ class Files:
                 print(f"Permission error moving {file} from \
                       {self.source_path} to {self.destination_path}")
             except IOError:
-                os.umask(0)
+                os.umask(0) # set up 755 chmod permission on created directory
                 os.makedirs(os.path.dirname(f"{self.destination_path}/{file}"), 
                             exist_ok=True)       
                 shutil.move(f"{self.source_path}/{file}", 
--- src/files_organizer_colo993/filters.py
+++ src/files_organizer_colo993/filters.py
@@ -15,16 +15,16 @@ class FilesList:
     def __init__(self, source_path):
         self.source_path = source_path
     
+    def filter_by_name(self, file_name_to_search) -> list:
+        """Find the files which matches the given word."""
+        return [file for file in os.listdir(path=self.source_path) if 
+                    file_name_to_search in file and file_name_to_search]
+    
     def filter_by_extension(self, type_of_extension) -> list: 
         """Select extension of a file"""   
         return [file for file in os.listdir(path=self.source_path) if 
                 re.search(f"{get_extensions_data()[type_of_extension]}$",
                           file.lower())]
-    
-    def filter_by_name(self, file_name_to_search) -> list:
-        """Find the files which matches the given word."""
-        return [file for file in os.listdir(path=self.source_path) if 
-                    file_name_to_search in file and file_name_to_search]
     
     def filter_by_date_creation(self, from_time, to_time) -> list:
         """Find the files created in between the given datetime range."""
@@ -38,11 +38,11 @@ class FilesList:
                 files_in_datetime_range.append(file)  
         return files_in_datetime_range
 
-    def get(self, option, extensions=None, names=None, dates=None) -> list:
+    def get(self, option, names=None, extensions=None, dates=None) -> list:
         """Return files present in all lists or files from all lists
         filtered by one or multiple conditions.
         """
-        entry_list = [extensions, names, dates] 
+        entry_list = [names, extensions, dates] 
         filtered_list = [x for x in entry_list if x is not None]
         if option == 'intersection':
             return list(set.intersection(*map(set,filtered_list)))
